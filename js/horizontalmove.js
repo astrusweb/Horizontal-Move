fec13b8639f3574b9a0cc8fa9fbed1cf30227491
Jonas Medeiros <comercial@astrusweb.com>
2011-05-27 09:01:19 -0300

    Versao 1.4.2

--- js/horizontalmove.js
+++ js/horizontalmove.js
@@ -1,5 +1,5 @@
 /*
- * jQuery Horizontal Move Plugin Beta 1.2
+ * jQuery Horizontal Move Plugin Beta 1.4.2
  * http://astrusweb.com/
  *
  * Copyright 2011 Astrusweb.com
@@ -17,17 +17,23 @@
         var page = !options.page ? 0 : options.page;
         var time = !options.time ? 1200 : options.time;
         var ease = !options.ease ? 'easeInOutQuart' : options.ease;
-
+        
+        options.main = !options.main ? '.horizontalmove-main:first' : options.main;
+        options.mask = !options.mask ? '.horizontalmove-mask:first' : options.mask;
+        options.item = !options.item ? '.horizontalmove-item' : options.item;        
+        options.left = !options.left ? '.horizontalmove-left' : options.left;
+        options.right = !options.right ? '.horizontalmove-right' : options.right;
+                
         var main_width = 0,
             calc = 0,
             rest = 0;
         
         // cache
         var $this = $(this);
-        var main = $this.find('.horizontalmove-main:first');
-        var mask = $this.find('.horizontalmove-mask:first');
+        var main = $this.find(options.main);
+        var mask = $this.find(options.mask);
         
-        $this.find('.horizontalmove-item').each(function(){
+        $this.find(options.item).each(function(){
             main_width += $(this).width();
         });
         main.width(main_width);
@@ -55,6 +61,12 @@
         }
         
         var move = function(){
+            
+            try
+            {
+              !options.callBack ? '' : options.callBack(page);
+            }catch(e){}
+          
             main.stop().animate({
                 left: - movements[page]
             }, time, ease,function(){!options.onComplete ? '' : options.onComplete(true)});
@@ -65,21 +77,33 @@
           move(page);
         }
         
-        $this.find('.horizontalmove-left').click(function(){
+        $this.find(options.left).click(function(){
             page--;
-            if (page < 0) {
-                page = 0;
-            }
-            move(page);
+            setPage(page);
         });
 
-        $this.find('.horizontalmove-right').click(function(){
-            page++;
-            if (page >= movements.length) {
-                page = movements.length - 1;
-            }
-            move(page);
+        $this.find(options.right).click(function(){
+            page++;       
+            setPage(page);
         });
+        
+        var setPage = function(p){
+          page = p;            
+          
+          if (page < 0) 
+          {
+            page = 0;
+          }else if (page >= movements.length) 
+          {
+            page = movements.length - 1;
+          }
+          
+          move(page);            
+        }
+        
+        return {
+          setPage : setPage
+        }
     };
 
 })(jQuery);
